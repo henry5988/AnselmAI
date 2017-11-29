bacf0d0179b29c773821cdbfe94ae82ed5da5ba3
Henry Chang <henry5988@hotmail.com>
2017-11-28 19:13:49 -0800

    Added a Constants interface

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+interface Constants{
+  //database constants
+  static final String USERNAME = "agile";
+  static final String PASSWORD = "tartan";
+  static final String URL = "jdbc:oracle:thin:@win-ooi3viu801v:1521:agile9";
+
+  // utility constants
+  static final int GETFILEEVENTTYPE = 2000011562;
+  static final int CHECKOUTFILEEVENTTYPE = 2000011559;
+}
