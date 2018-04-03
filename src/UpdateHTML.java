2d33b885868d13cfd6ff8a6c82d788108c496a70
Henry Chang <henry5988@hotmail.com>
2018-04-02 19:24:09 -0700

    Changed UpdateHTML to be a OO class

--- src/UpdateHTML.java
+++ src/UpdateHTML.java
@@ -5,8 +5,12 @@ import java.io.FileReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
-public class updateHTML {
-		 public static void main(final String[] args) throws Exception {
+public class UpdateHTML {
+	   private String txtDirectory;
+	   private String templateDirectory;
+	   private String outputDirectory;
+
+		 public void update(final String[] args) throws Exception {
 		
 			 String line;
 			 String html = "";
@@ -14,17 +18,17 @@ public class updateHTML {
 			 int br_data_line = 0;
 			 int i = 0;
 			 
-			 String data_path = "C:\\Users\\user\\Desktop\\documentsPopup_data.txt";//資料文件路徑
+			 String data_path = getTxtDirectory();//資料文件路徑
 			 FileReader fr_data= new FileReader(data_path);
 			 BufferedReader br_data = new BufferedReader(fr_data);
 			
-			 String html_path = "C:\\Users\\user\\Desktop\\documentsPopup.txt";//網頁文件路徑
+			 String html_path = getTemplateDirectory();//網頁文件路徑
 			 FileReader fr_html= new FileReader(html_path);
 			 BufferedReader br_html = new BufferedReader(fr_html);
 			 StringBuffer sb = new StringBuffer();
 			 
 			 //輸出網頁名稱與編碼設定
-			 BufferedWriter fw  = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\user\\Desktop\\documentsPopup.html"), "UTF8"));
+			 BufferedWriter fw  = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(getOutputDirectory()), "UTF8"));
 	
 
 			 while ((line = br_data.readLine()) != null) { //逐行讀取txt檔，該行的內容存進line
@@ -37,15 +41,15 @@ public class updateHTML {
 				 if( line.indexOf("{(anselmai)}")!=-1) { //判斷該行是否含有關鍵字
 					 if (tempArray[i].equals("{(anselmai)}")) {
 						 tempArray[i] = "";
-						 html += line.replace("{(anselmai)}",tempArray[i])+"\n";	
+						 html += line.replace("{(anselmai)}",tempArray[i])+String.format("%n");
 					 }
 					 else {
-						 html += line.replace("{(anselmai)}",tempArray[i])+"\n";
+						 html += line.replace("{(anselmai)}",tempArray[i])+String.format("%n");
 					 }
 					 i++;
 				 }
 				 else {
-					 html += line+"\n"; 
+					 html += line+String.format("%n");
 				 }	 
 			 }
 			 fw.write(html);//將暫存內容寫進檔案中
@@ -56,5 +60,29 @@ public class updateHTML {
 			 br_html.close();
 			 
 			  }
-		 }
+
+	public String getTxtDirectory() {
+		return txtDirectory;
+	}
+
+	public void setTxtDirectory(String txtDirectory) {
+		this.txtDirectory = txtDirectory;
+	}
+
+	public String getTemplateDirectory() {
+		return templateDirectory;
+	}
+
+	public void setTemplateDirectory(String templateDirectory) {
+		this.templateDirectory = templateDirectory;
+	}
+
+	public String getOutputDirectory() {
+		return outputDirectory;
+	}
+
+	public void setOutputDirectory(String outputDirectory) {
+		this.outputDirectory = outputDirectory;
+	}
+}
 
